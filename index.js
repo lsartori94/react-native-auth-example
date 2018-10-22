8febba2b4becf81c1841584796cb4d2b38f85dca
Luca Sartori <luca.x.sartori.-nd@disney.com>
2018-10-22 13:02:50 -0300

    adding example

---
+++ index.js
@@ -0,0 +1,24 @@
+/** @format */
+import React from 'react'
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+// redux
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './src/reducers'
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+// Amplify
+import config from './src/aws-exports'
+import Amplify from 'aws-amplify'
+Amplify.configure(config);
+// App
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
